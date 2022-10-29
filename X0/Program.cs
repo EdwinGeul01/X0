c53a2ff876650bc6cab750f3343c891ecaf18dfc
Edwin Geul <edwingeul01@gmail.com>
2022-10-29 00:11:01 -0600

    Agregar archivos de proyecto.

---
+++ X0/Program.cs
@@ -0,0 +1,418 @@
+﻿
+
+interface Game
+{
+    void StartGame();
+}
+
+class GameManager : Game
+{
+    string[] boardPositions = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+    bool player1Turn = true;
+    bool player2Turn = false;
+
+
+
+    private void DrawBoard()
+    {
+
+        Console.WriteLine("------------");
+        Console.WriteLine(" " + boardPositions[0] + " | "+ boardPositions[1] +" | " + boardPositions[2] );
+        Console.WriteLine("------------");
+        Console.WriteLine(" " + boardPositions[3] + " | " + boardPositions[4] + " | " + boardPositions[5]);
+        Console.WriteLine("------------");
+        Console.WriteLine(" " + boardPositions[6] + " | " + boardPositions[7] + " | " + boardPositions[8]);
+        Console.WriteLine("------------");
+
+
+
+    }
+
+
+    private void SelectRegion()
+    {
+        bool BusyRegion = false;
+        int RegionSelected = -1;
+        do
+        {
+
+            Console.WriteLine("Seleccione la region donde desea pintar : ");
+            try
+            {
+                RegionSelected = int.Parse(Console.ReadLine());
+                if (RegionSelected > 0 && RegionSelected <= 9)
+                {
+                    if (boardPositions[RegionSelected - 1] == "X" || boardPositions[RegionSelected - 1] == "0")
+                    {
+                        Console.WriteLine("Invalid Position ~ ");
+                        BusyRegion = true;
+                    }
+                    else
+                    {
+                        if (player1Turn)
+                            boardPositions[RegionSelected-1] = "0";
+                        else
+                            boardPositions[RegionSelected-1] = "X";
+
+                        BusyRegion = false;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Position");
+                    BusyRegion = true;
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                BusyRegion = false;
+            }
+
+
+        } while (BusyRegion);
+
+
+
+    }
+
+
+    private void ChangePlayer()
+    {
+
+        if(player1Turn)
+        {
+            player2Turn = true;
+            player1Turn = false;
+        }
+        else if (player2Turn)
+        {
+            player2Turn = false ;
+            player1Turn = true;
+        }
+
+
+    }
+
+
+    private void StartTurn()
+    {
+
+        SelectRegion();
+        ChangePlayer();
+
+
+
+    }
+
+
+    private bool CheckWinner()
+    {
+        String Character = "X";
+        for (int i = 0; i < 2; i++)
+        {
+            if (boardPositions[0] == Character && boardPositions[1] == Character && boardPositions[2] == Character)
+                return true;
+            else if (boardPositions[3] == Character && boardPositions[4] == Character && boardPositions[5] == Character)
+                return true;
+            else if (boardPositions[6] == Character && boardPositions[7] == Character && boardPositions[8] == Character)
+                return true;
+            else if (boardPositions[0] == Character && boardPositions[4] == Character && boardPositions[8] == Character)
+                return true;
+            else if (boardPositions[2] == Character && boardPositions[4] == Character && boardPositions[6] == Character)
+                return true;
+
+            Character = "0";
+        }
+        return false;
+
+
+    }
+
+    private bool CheckTie()
+    {
+        foreach(string position in boardPositions)
+        {
+            if(position != "X" && position != "0")
+                return false;
+        }
+
+
+
+        return true;
+
+    }
+
+
+    public void StartGame()
+    {
+        Console.WriteLine("Starting the game !! ");
+        bool HasFinished = false;
+        bool HasTie = false;
+
+
+        while (!HasFinished && !HasTie)
+        {
+            DrawBoard();
+            StartTurn();
+            HasFinished = CheckWinner();
+            HasTie = CheckTie();
+            Console.Clear();
+
+        }
+
+        if(HasTie)
+        {
+            Console.WriteLine("Partido empatado");
+        }
+        else if (HasFinished)
+        {
+            if(!player1Turn)
+            {
+                Console.WriteLine("Jugador 1 Gano");
+            }else
+            {
+                Console.WriteLine("Jugador 2 Gano");
+            }
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+/*
+ * 
+Prefijos y miembros
+Asignaciones mentales
+Nombres de clases -> 
+Nombres de métodos -> 
+No exceder el atractivo
+Abreviaturas y simetrías
+ 
+ */
+class C_Connect
+{
+    string[] V_Pos = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; // Positions on the board
+    bool p1T = true;
+    bool p2T = false;
+
+
+
+    private void M_DrawBoard()
+    {
+
+        Console.WriteLine("------------");
+        Console.WriteLine(" " + V_Pos[0] + " | " + V_Pos[1] + " | " + V_Pos[2]);
+        Console.WriteLine("------------");
+        Console.WriteLine(" " + V_Pos[3] + " | " + V_Pos[4] + " | " + V_Pos[5]);
+        Console.WriteLine("------------");
+        Console.WriteLine(" " + V_Pos[6] + " | " + V_Pos[7] + " | " + V_Pos[8]);
+        Console.WriteLine("------------");
+
+
+
+    }
+
+
+    private void M_SelectRegion()
+    {
+        bool BusyRegion = false;
+        int RegionSelected = -1;
+        do
+        {
+
+            Console.WriteLine("Seleccione la region donde desea pintar : ");
+            try
+            {
+                RegionSelected = int.Parse(Console.ReadLine());
+                if (RegionSelected > 0 && RegionSelected <= 9)
+                {
+                    if (V_Pos[RegionSelected - 1] == "X" || V_Pos[RegionSelected - 1] == "0")
+                    {
+                        Console.WriteLine("Invalid Position ~ ");
+                        BusyRegion = true;
+                    }
+                    else
+                    {
+                        if (p1T)
+                            V_Pos[RegionSelected - 1] = "0";
+                        else
+                            V_Pos[RegionSelected - 1] = "X";
+
+                        BusyRegion = false;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Position");
+                    BusyRegion = true;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                BusyRegion = false;
+            }
+
+
+        } while (BusyRegion);
+
+
+
+    }
+
+
+    private void M_ChangePlayer()
+    {
+
+        if (p1T)
+        {
+            p2T = true;
+            p1T = false;
+        }
+        else if (p2T)
+        {
+            p2T = false;
+            p1T = true;
+        }
+
+
+    }
+
+
+    private void M_StartTurn()
+    {
+
+        M_SelectRegion();
+        M_ChangePlayer();
+
+
+
+    }
+
+
+    private bool M_CheckWinner()
+    {
+        String Character = "X";
+        for (int i = 0; i < 2; i++)
+        {
+            if (V_Pos[0] == Character && V_Pos[1] == Character && V_Pos[2] == Character)
+                return true;
+            else if (V_Pos[3] == Character && V_Pos[4] == Character && V_Pos[5] == Character)
+                return true;
+            else if (V_Pos[6] == Character && V_Pos[7] == Character && V_Pos[8] == Character)
+                return true;
+            else if (V_Pos[0] == Character && V_Pos[4] == Character && V_Pos[8] == Character)
+                return true;
+            else if (V_Pos[2] == Character && V_Pos[4] == Character && V_Pos[6] == Character)
+                return true;
+
+            Character = "0";
+        }
+        return false;
+
+
+    }
+
+    private bool M_CheckTie()
+    {
+        foreach (string position in V_Pos)
+        {
+            if (position != "X" && position != "0")
+                return false;
+        }
+
+
+
+        return true;
+
+    }
+
+
+    public void M_StartGame()
+    {
+        Console.WriteLine("Starting the game !! ");
+        bool HasFinished = false;
+        bool HasTie = false;
+
+
+        while (!HasFinished && !HasTie)
+        {
+            M_DrawBoard();
+            M_StartTurn();
+            HasFinished = M_CheckWinner();
+            HasTie = M_CheckTie();
+            Console.Clear();
+
+        }
+
+        if (HasTie)
+        {
+            Console.WriteLine("Partido empatado");
+        }
+        else if (HasFinished)
+        {
+            if (!p1T)
+            {
+                Console.WriteLine("Jugador 1 Gano");
+            }
+            else
+            {
+                Console.WriteLine("Jugador 2 Gano");
+            }
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+class MainClass
+{
+
+
+
+    public static void Main(string[] args)
+    {
+
+        GameManager GameManager = new GameManager();
+
+        GameManager.StartGame();
+
+
+
+
+
+
+
+    }
+
+
+}
